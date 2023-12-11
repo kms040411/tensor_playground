ff3a224af1905a834e2c62c4a22c5b304b7523bb
mskim <mskim@casys.kaist.ac.kr>
2023-12-11 17:07:42 +0900

    .

--- ops.py
+++ ops.py
@@ -1,5 +1,7 @@
 import torch
-import progressbar
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
 
 def print_tensor(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
     return tensor_a, tensor_b
@@ -9,6 +11,10 @@ def clear(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
 
 def swap(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
     return tensor_b, tensor_a
+
+def fwrite(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
+    pd.DataFrame(tensor_a.numpy()).to_csv("tensor_a.csv", index=False, header=False)
+    return tensor_a, tensor_b
 
 def print_dist(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
     if tensor_a is None:
@@ -38,26 +44,24 @@ def print_dist(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
         counts.append(0)
     
     flattened = tensor_a.reshape(-1).tolist()
+    flattened = list(filter(lambda x: x != 0.0, flattened)) # Remove 0.0f
     total_elem = len(flattened)
     print("total elements: ", total_elem)
 
-    bar = progressbar.ProgressBar(maxval=total_elem).start()
-    for idx, i in enumerate(flattened):
-        bar.update(idx)
+    for i in tqdm(flattened, leave=False):
         reach_end = True
         for idx in range(num_bin - 1):
             if bins[idx] <= i < bins[idx + 1]:
                 counts[idx] += 1
                 reach_end = False
         if reach_end: counts[num_bin - 1] += 1
-    bar.finish()
     
     for i in range(num_bin):
-        print(f"{counts[i] * 1.0 / total_elem:.4f}\t", end="")
-    print()
-    for i in range(num_bin):
-        print(f"{bins[i]:.4f}\t", end="")
-    print()
+        print(f"{bins[i]:6.4f}\t", end="")
+        print(f"{counts[i] * 1.0 / total_elem:8.6f}\t", end="")
+        for _ in range(int(counts[i] * 1.0 / total_elem * 500)):
+            print("#", end="")
+        print("")
 
     return tensor_a, tensor_b
 
@@ -72,3 +76,13 @@ def qr_decomposition(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
     
     q, r = torch.linalg.qr(tensor_a.float(), mode=user_option)
     return q.half(), r.half()
+
+def lu_decomposition(tensor_a: torch.Tensor, tensor_b: torch.Tensor):
+    user_ok = input("This function will erase tensor_b. Are you okay to continue? [y/N]: ")
+    if user_ok not in ("y", "Y"):
+        return tensor_a, tensor_b
+
+    p, l, u = torch.linalg.lu(tensor_a.float(), pivot=True)
+    print("Pivot Matrix: ")
+    print(p)
+    return l.half(), u.half()
